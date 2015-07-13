fc4f8806f3dd5cc080d4e1e056e6cbd1c2330903
Maria Remolina-Gutierrez <remolina@pczaal4.strw.leidenuniv.nl>
2015-07-13 19:47:34 +0200

    just before trying a super long term evolution

---
+++ Experiment_WD0806-661B/long_term_evolution/ffp.py
@@ -0,0 +1,414 @@
+import math, numpy, argparse, os
+
+from matplotlib.pyplot import *
+from sympy.solvers import solve
+from sympy import Symbol
+
+from amuse.io import write_set_to_file
+from amuse.io import base
+from amuse.units import units, constants, nbody_system
+from amuse.units.quantities import AdaptingVectorQuantity
+from amuse.datamodel import Particles, ParticlesSuperset
+from amuse.community.smalln.interface import SmallN
+from amuse.ext.orbital_elements import new_binary_from_orbital_elements, orbital_elements_from_binary
+
+def initialize_code(bodies, code=SmallN, timestep_parameter=0.0169):
+    """
+    initialize gravity code for bodies
+    """
+    stars_gravity = code()
+    stars_gravity.particles.add_particles(bodies)
+    stars_gravity.commit_particles()
+    stars_gravity.parameters.timestep_parameter = timestep_parameter  # default 0.0169 time
+
+    return stars_gravity
+
+def my_orbital_elements_from_binary(binary, G=nbody_system.G):
+
+    if len(binary)>2:
+      raise Exception("expects binary or single part")
+
+    if len(binary)==2:
+      mass1=binary[0].mass
+      mass2=binary[1].mass
+      position = binary[1].position-binary[0].position
+      velocity = binary[1].velocity-binary[0].velocity
+      total_mass = mass1 + mass2
+    else:
+      mass1=binary.mass
+      mass2=0.*mass1
+      position = binary.position
+      velocity = binary.velocity
+      total_mass = mass1
+
+    specific_energy = (1.0/2.0)*velocity.lengths_squared() - G*total_mass/position.lengths()
+    specific_angular_momentum = position.cross(velocity)
+    specific_angular_momentum_norm = specific_angular_momentum.lengths()
+
+    semimajor_axis = -G*total_mass/(2.0*specific_energy)
+
+    eccentricity_argument = 2.0*specific_angular_momentum_norm**2*specific_energy/(G**2*total_mass**2)
+    if (eccentricity_argument <= -1): eccentricity = 0.0
+    else: eccentricity = numpy.sqrt(1.0 + eccentricity_argument)
+
+    return mass1, mass2, semimajor_axis, eccentricity
+
+def get_parabolic_velocity(m0, m_ffp, b_ffp, r_inf, m_bp, a_bp, phi_bp):
+
+    cm_M = m0 + m_bp
+    cm_x = m_bp*a_bp*math.cos(np.radians(phi_bp))/cm_M
+    cm_y = m_bp*a_bp*math.sin(np.radians(phi_bp))/cm_M
+
+    M = m0 + m_ffp + m_bp
+    r = ((r_inf-cm_x)**2+(b_ffp-cm_y)**2).sqrt()
+
+    parabolic_velocity_squared = 2.0*M*(1.0 | nbody_system.length**3 * nbody_system.time**-2 * nbody_system.mass**-1)/r
+
+    return parabolic_velocity_squared.sqrt()
+
+def get_bodies_in_orbit(m0, m_ffp, m_bp, a_bp, e_bp, phi_bp, b_ffp, r_inf):
+
+    #Bodies
+    bodies = Particles()
+
+    ##Get BP in orbit
+    #Binary
+    star_planet = new_binary_from_orbital_elements(m0, m_bp, a_bp, e_bp, true_anomaly=phi_bp)
+    #Planet attributes
+    star_planet.eccentricity = e_bp
+    star_planet.semimajoraxis = a_bp
+    #Center on the star
+    star_planet.position -= star_planet[0].position
+    star_planet.velocity -= star_planet[0].velocity
+    cm_p = star_planet.center_of_mass()
+    cm_v = star_planet.center_of_mass_velocity()
+
+    ##Get FFP in orbit
+    #Particle set
+    m0_ffp = Particles(2)
+    #Zeros and parabolic velocity
+    zero_p = 0.0 | nbody_system.length
+    zero_v = 0.0 | nbody_system.speed
+    parabolic_velocity = get_parabolic_velocity(m0, m_ffp, b_ffp, r_inf, m_bp, a_bp, phi_bp)
+    #Central star
+    m0_ffp[0].mass = m0
+    m0_ffp[0].position = (zero_p,zero_p,zero_p)
+    m0_ffp[0].velocity = (zero_v,zero_v,zero_v)
+    #Free-floating planet
+    m0_ffp[1].mass = m_ffp
+    m0_ffp[1].position = (-r_inf-cm_p[0],-b_ffp-cm_p[1],zero_p)
+    m0_ffp[1].velocity = (parabolic_velocity,zero_v,zero_v)
+    #Orbital Elements
+    star_planet_as_one = Particles(1)
+    star_planet_as_one.mass = m0 + m_bp
+    star_planet_as_one.position = cm_p
+    star_planet_as_one.velocity = cm_v
+    binary = [star_planet_as_one[0], m0_ffp[1]]
+    m1, m2, sma, e = my_orbital_elements_from_binary(binary)
+    #For the star it sets the initial values of semimajoraxis and eccentricity of the ffp around star+bp
+    m0_ffp.eccentricity = e
+    m0_ffp.semimajoraxis = sma
+
+    #Order: star, ffp, bp
+    bodies.add_particle(m0_ffp[0])
+    bodies.add_particle(m0_ffp[1])
+    bodies.add_particle(star_planet[1])
+
+    return bodies
+
+def save_particles_to_file(bodies, bodies_to_save, bodies_filename,time,converter):
+    #Add attributes that I'm interested in to the bodies_to_save
+    bodies_to_save.position = converter.to_si(bodies.position).as_quantity_in(units.AU)
+    bodies_to_save.velocity = converter.to_si(bodies.velocity).as_quantity_in(units.kms)
+    bodies_to_save.semimajoraxis = converter.to_si(bodies.semimajoraxis).as_quantity_in(units.AU)
+    bodies_to_save.eccentricity = bodies.eccentricity
+    bodies_to_save.time = converter.to_si(time).as_quantity_in(units.yr)
+
+    write_set_to_file(bodies_to_save, bodies_filename, "hdf5")
+
+def solve_for_x(m0, m_bp, m_ffp):
+
+    coefficients = [m_bp+m_ffp, 2*m_bp+3*m_ffp, m_bp+3*m_ffp, -(m_bp+3*m0), -(3*m0+2*m_bp), -(m0+m_bp)]
+    solutions = numpy.roots(coefficients)
+
+    return abs(solutions[math.ceil(len(solutions)/2.0)-1])
+
+def is_hill_stable(m_values, a_values, e_values, converter):
+
+    m0 = m_values[0].value_in(nbody_system.mass)
+    m_ffp = m_values[1].value_in(nbody_system.mass)
+    m_bp = m_values[2].value_in(nbody_system.mass)
+
+    M = m0 + m_bp + m_ffp
+    mu = m0 + m_bp
+
+    a_1 = a_values[2].value_in(nbody_system.length)
+    a_2 =a_values[1].value_in(nbody_system.length)
+
+    e_1 = e_values[2]
+    e_2 = e_values[1]
+
+    x = solve_for_x(m0, m_bp, m_ffp)
+
+    f_x = m0*m_bp + (m0*m_ffp)/(1+x) + (m_bp*m_ffp)/(x)
+    g_x = m_bp*m_ffp + m0*m_ffp*(1+x)**2 + m0*m_bp*(x**2)
+
+    A = -(f_x**2)*g_x/(m_ffp**3 * mu**3 * (1-e_2**2))
+
+    if ((1-e_1**2)/(1-e_2**2) < 0.0):
+        return False
+    if ((a_1*m_ffp*mu)/(a_2*m0*m_bp) < 0.0):
+        return False
+
+    beta = (m0*m_bp/m_ffp)**(3.0/2.0)*(M/(mu**4))**(1.0/2.0)*((1-e_1**2)/(1-e_2**2))**(1.0/2.0)
+    y = ((a_1*m_ffp*mu)/(a_2*m0*m_bp))**(1.0/2.0)
+
+    equation = (1+y**2)*(beta**2*y**2 + 2*beta*y + 1) - A*y**2
+
+    if(equation >= 0.0):
+        return True
+    else:
+        return False
+
+def evolve_gravity(bodies, number_of_planets, converter, t_end, n_steps, n_snapshots, bodies_filename):
+
+    #Particles that will be saved in the file
+    bodies_to_save = Particles(number_of_planets+2)
+
+    #Positions and velocities centered on the center of mass
+    bodies.move_to_center()
+
+    time = 0. | nbody_system.time
+    dt = t_end / float(n_steps)
+    dt_snapshots = t_end / float(n_snapshots)
+
+    gravity = initialize_code(bodies, timestep_parameter = dt.value_in(nbody_system.time))
+    channel_from_gr_to_framework = gravity.particles.new_channel_to(bodies)
+
+    x = AdaptingVectorQuantity()
+    y = AdaptingVectorQuantity()
+    times = AdaptingVectorQuantity()
+    system_energies = AdaptingVectorQuantity()
+
+    E_initial = gravity.kinetic_energy + gravity.potential_energy
+    DeltaE_max = 0.0 | nbody_system.energy
+
+    while time<=t_end:
+
+        gravity.evolve_model(time)
+        channel_from_gr_to_framework.copy()
+
+        bodies.collection_attributes.timestamp = time
+
+        gravity.particles.collection_attributes.timestamp = time
+
+        x.append(bodies.x)
+        y.append(bodies.y)
+        times.append(time)
+
+        E = gravity.kinetic_energy + gravity.potential_energy
+        system_energies.append(E)
+
+        DeltaE = abs(E-E_initial)
+        if ( DeltaE > DeltaE_max ):
+            DeltaE_max = DeltaE
+
+        #Orbital Elements
+        star = bodies[0]
+        ffp = bodies[1]
+        bp = bodies[2]
+
+        #Star and FFP
+        binary = [star, ffp]
+        m1, m2, sma_star_ffp, e_star_ffp = my_orbital_elements_from_binary(binary)
+        bodies[1].eccentricity = e_star_ffp
+        bodies[1].semimajoraxis = sma_star_ffp
+
+        #Star and BP
+        binary = [star, bp]
+        m1, m2, sma_star_bp, e_star_bp = my_orbital_elements_from_binary(binary)
+        bodies[2].eccentricity = e_star_bp
+        bodies[2].semimajoraxis = sma_star_bp
+
+        save_particles_to_file(bodies, bodies_to_save, bodies_filename,time,converter)
+
+        time += dt_snapshots
+
+    #To get last orbital elements
+    #Star and FFP
+    binary = [star, ffp]
+    b1_mass1, b1_mass2, b1_semimajor_axis, b1_eccentricity, b1_true_anomaly, b1_inclination, b1_long_asc_node, b1_arg_per = my_orbital_elements_from_binary(binary)
+    #Star and BP
+    binary = [star, bp]
+    b2_mass1, b2_mass2, b2_semimajor_axis, b2_eccentricity, b2_true_anomaly, b2_inclination, b2_long_asc_node, b2_arg_per = my_orbital_elements_from_binary(binary)
+
+    #To check Hill stability
+    #Star+BP and FFP
+    cm_x = (star.mass*star.x + bp.mass*bp.x)/(star.mass+bp.mass)
+    cm_y = (star.mass*star.y + bp.mass*bp.y)/(star.mass+bp.mass)
+    cm_vx = (star.mass*star.vx + bp.mass*bp.vx)/(star.mass+bp.mass)
+    cm_vy = (star.mass*star.vy + bp.mass*bp.vy)/(star.mass+bp.mass)
+
+    star_bp = Particles(1)
+    star_bp.mass = star.mass + bp.mass
+    star_bp.position = [cm_x, cm_y, 0.0 | nbody_system.length]
+    star_bp.velocity = [cm_vx, cm_vy, 0.0 | nbody_system.speed]
+
+    binary = [star_bp[0], ffp]
+    m1, m2, sma_starbp_ffp, e_starbp_ffp = my_orbital_elements_from_binary(binary)
+    bodies[1].eccentricity = e_starbp_ffp
+    bodies[1].semimajoraxis = sma_starbp_ffp
+
+    max_energy_change = DeltaE_max/E_initial
+
+    is_stable = is_hill_stable(bodies.mass, bodies.semimajoraxis, bodies.eccentricity, converter)
+
+    gravity.stop()
+
+    return x,y,times,system_energies,max_energy_change,is_stable,converter.to_si(b1_semimajor_axis).value_in(units.AU), b1_eccentricity, b1_true_anomaly, b1_inclination, b1_long_asc_node, b1_arg_per, converter.to_si(b2_semimajor_axis).value_in(units.AU), b2_eccentricity, b2_true_anomaly, b2_inclination, b2_long_asc_node, b2_arg_per
+
+def plot_trajectory(x,y,number_of_planets, filename):
+
+    colors = ['magenta', 'green', 'DarkOrange', 'red']
+
+    f=figure(figsize=(35,15))
+
+    x_star = x[:,0].value_in(nbody_system.length)
+    x_ffp = x[:,1].value_in(nbody_system.length)
+
+    y_star = y[:,0].value_in(nbody_system.length)
+    y_ffp = y[:,1].value_in(nbody_system.length)
+
+    plot(x_star,y_star,'y',label='Star')
+    scatter(x_star[0],y_star[0],c='black',marker='*')
+    scatter(x_star[-1],y_star[-1],c='y',marker='*')
+
+    plot(x_ffp,y_ffp,'c',label='FFP')
+    scatter(x_ffp[0],y_ffp[0],c='black')
+    scatter(x_ffp[-1],y_ffp[-1],c='c')
+
+    for i in range(0, number_of_planets):
+
+        x_planet = x[:,i+2].value_in(nbody_system.length)
+        y_planet = y[:,i+2].value_in(nbody_system.length)
+
+        color_planet = colors[i]
+
+        plot(x_planet,y_planet,color=color_planet,label='BP',alpha=0.5)
+        scatter(x_planet[0],y_planet[0],c='black')
+        scatter(x_planet[-1],y_planet[-1],color=color_planet)
+
+    axhline(y=0, xmin=-80, xmax=10, c='black', linestyle='--')
+    axvline(x=0, ymin=-5, ymax=2, c='black', linestyle='--')
+
+    title('Trajectory FFP (nbody units)')
+    xlabel("$x$", fontsize=20)
+    ylabel("$y$", fontsize=20)
+    legend()
+
+    savefig('trajectory_'+filename+'.png')
+
+    xlim(-3.1,1.1)
+    ylim(-1,1)
+
+    close()
+
+def plot_energy_change(times, energies,filename):
+
+    times = times.value_in(nbody_system.time)
+    energies = energies.value_in(nbody_system.energy)
+
+    initial_energy = energies[0]
+    energies = (energies-initial_energy)/initial_energy
+
+    f=figure(figsize=(15,15))
+
+    plot(times,numpy.log10(abs(energies)),color='black')
+
+    axhline(y=0, xmin=0, xmax=times[-1], c='m', linestyle='--')
+
+    title('Total Energy of the System (nbody units)')
+    xlabel("$t$", fontsize=20)
+    ylabel("$\log{(|\Delta E / E|)}$", fontsize=20)
+
+    savefig('energy_'+filename+'.png')
+    close()
+
+def convert_units(converter, t_end_p, m0_p, m_ffp_p, e_bp_p, m_bp_p, a_bp_p, b_ffp_p, phi_bp_p):
+
+    #time of integration in yr
+    t_end = converter.to_nbody(t_end_p | units.yr)
+    #mass of the disk-central star in MSun
+    m0 = converter.to_nbody(m0_p | units.MSun)
+    #mass of the FFP MJupiter
+    m_ffp = converter.to_nbody(m_ffp_p | units.MJupiter)
+    #eccentricity of the bounded planets
+    e_bp = e_bp_p
+    #mass of the bounded planets in MJupiter
+    m_bp = converter.to_nbody(m_bp_p | units.MJupiter)
+    #semimajor axis of the bounded planets in AU
+    a_bp = converter.to_nbody(a_bp_p | units.AU)
+    #impact parameter of the FFP in AU
+    b_ffp = converter.to_nbody(b_ffp_p | units.AU)
+    #initial angle for of the bounded planet in degrees
+    phi_bp = phi_bp_p
+
+    return t_end, m0, m_ffp, e_bp, m_bp, a_bp, b_ffp, phi_bp
+
+def stop_code():
+    print '\nSTOP'
+    import sys
+    sys.exit()
+
+def run_capture(t_end_p=650.0, m0_p=0.58, m_ffp_p=7.5, e_bp_p=0.0, m_bp_p=0.1, a_bp_p=1.0, b_ffp_p=1.0, phi_bp_p=0.0, n_steps=10000, path_filename='./particles/m/m_a_b_p_.hdf5', n_snapshots=500):
+    """
+    Units: t_end_p(yr), m0_p(MSun), m_ffp_p(MJupiter), e_bp_p(None), m_bp_p(MJupiter), a_bp_p(AU), b_ffp(AU), phi_p(degrees)
+    """
+
+    #Converter used in this program
+    converter = nbody_system.nbody_to_si(1 | units.MSun,  5 | units.AU)
+
+    #Conversion of units
+    t_end, m0, m_ffp, e_bp, m_bp, a_bp, b_ffp, phi_bp = convert_units(converter, t_end_p, m0_p, m_ffp_p, e_bp_p, m_bp_p, a_bp_p, b_ffp_p, phi_bp_p)
+
+    #Number of planets
+    number_of_planets = 1
+    #Initial distance to the planet (x-cordinate)
+    r_inf = 40.0*a_bp
+
+    #Particle superset: star, FFP, planets
+    bodies = get_bodies_in_orbit(m0, m_ffp, m_bp, a_bp, e_bp, phi_bp, b_ffp, r_inf)
+
+    #Evolve time
+    x,y,times,system_energies,max_energy_change,is_stable,b1_semimajor_axis, b1_eccentricity, b1_true_anomaly, b1_inclination, b1_long_asc_node, b1_arg_per, b2_semimajor_axis, b2_eccentricity, b2_true_anomaly, b2_inclination, b2_long_asc_node, b2_arg_per = evolve_gravity(bodies, number_of_planets, converter, t_end, n_steps, n_snapshots, path_filename)
+
+    name = 'm'+str(m0_p)+'_a'+str(a_bp_p)+'_b'+str(b_ffp_p)+'_p'+str(phi_bp_p)
+
+    print 'Name:', name
+    print 'Max Energy Change:', max_energy_change
+    print 'Is Hill stable?:', is_stable
+    print '\nBinary Star and FFP:'
+    print 'semimajor_axis(AU) eccentricity true_anomaly inclination longitude_of_the_ascending_node argument_of_periapsis'
+    print b1_semimajor_axis, b1_eccentricity, b1_true_anomaly, b1_inclination, b1_long_asc_node, b1_arg_per
+    print '\nBinary Star and BP:'
+    print 'semimajor_axis(AU) eccentricity true_anomaly inclination longitude_of_the_ascending_node argument_of_periapsis'
+    print b2_semimajor_axis, b2_eccentricity, b2_true_anomaly, b2_inclination, b2_long_asc_node, b2_arg_per
+
+if __name__ in ('__main__', '__plot__'):
+
+    #This one starts down, is similar to next one
+    m1 = 1.188889
+    a1 = 1.000000
+    b1 = 11.232833
+    p1 = 229.458778
+    f1 = 'm1.188889e+00_a1.000000e+00_b1.123283e+01_p2.294588e+02'
+
+    #This one is at -300 and seem to be going further
+    m2 = 1.188889
+    a2 = 1.000000
+    b2 = -8.023452
+    p2 = 275.241762
+    f2 = 'm1.188889e+00_a1.000000e+00_b-8.023452e+00_p2.752418e+02'
+
+    run_capture(t_end_p=65000.0*15, m0_p=0.58, m_ffp_p=7.5, e_bp_p=0.0, m_bp_p=m1, a_bp_p=a1, b_ffp_p=b1, phi_bp_p=p1, n_steps=1200000*15, path_filename='./'+f1+'.hdf5', n_snapshots=60000*15)
+    #run_capture(t_end_p=65000.0*15, m0_p=0.58, m_ffp_p=7.5, e_bp_p=0.0, m_bp_p=m2, a_bp_p=a2, b_ffp_p=b2, phi_bp_p=p2, n_steps=1200000*15, path_filename='./'+f2+'.hdf5', n_snapshots=60000*15)
--- Experiment_WD0806-661B/long_term_evolution/plots.py
+++ Experiment_WD0806-661B/long_term_evolution/plots.py
@@ -7,9 +7,8 @@ from sympy.solvers import solve
 from sympy import Symbol
 
 from amuse.support import io
-from amuse.units import units
+from amuse.units import units,constants
 from amuse.datamodel import Particles
-from amuse.ext.orbital_elements import orbital_elements_from_binary
 
 def stop_code():
     print '\nSTOP'
@@ -60,17 +59,16 @@ def is_hill_stable(m_values, a_values, e_values):
     else:
         return False
 
-def plot_trajectory(x, y, filename):
+def plot_trajectory(x, y, filename, x_start_longterm, y_start_longterm):
 
     f = plt.figure(figsize=(70,30))
 
     x_star = x[:,0]-x[:,0]
     x_ffp = x[:,1]-x[:,0]
+    x_planet = x[:,2]-x[:,0]
 
     y_star = y[:,0]-y[:,0]
     y_ffp = y[:,1]-y[:,0]
-
-    x_planet = x[:,2]-x[:,0]
     y_planet = y[:,2]-y[:,0]
 
     plt.plot(x_star,y_star,c='y',label='Star')
@@ -84,6 +82,9 @@ def plot_trajectory(x, y, filename):
     plt.plot(x_planet,y_planet,c='m',label='BP',alpha=0.5)
     plt.scatter(x_planet[0],y_planet[0],c='black', lw = 0)
     plt.scatter(x_planet[-1],y_planet[-1],c='m', lw = 0)
+
+    plt.scatter(x_start_longterm[1],y_start_longterm[1], c='c', lw = 0, marker='s')
+    plt.scatter(x_start_longterm[2],y_start_longterm[2], c='m', lw = 0, marker='s')
 
     plt.axhline(y=0, xmin=-80, xmax=10, c='black', linestyle='--')
     plt.axvline(x=0, ymin=-5, ymax=2, c='black', linestyle='--')
@@ -122,19 +123,100 @@ def plot_orbital_elements(times, eccs, smas, filename):
     #semimajoraxis
     subplot = f.add_subplot(1,2,2)
 
-    sma_starbp_ffp_log = [math.log10(s) for s in sma_starbp_ffp]
-    sma_star_bp_log = [math.log10(s) for s in sma_star_bp]
+    sma_starbp_ffp_log = []
 
-    subplot.plot(times,sma_starbp_ffp_log,c='red',label='FFP and Star+BP')
-    subplot.plot(times,sma_star_bp_log,c='green',label='BP and Star')
+    i=0
+    for s in sma_starbp_ffp:
+        if(i!=0):
+            try:
+                sma_starbp_ffp_log.append(math.log10(s))
+            except:
+                # print i
+                sma_starbp_ffp_log.append(sma_starbp_ffp_log[-1])
+        i += 1
+
+    sma_star_bp_log = []
+
+    i=0
+    for s in sma_star_bp:
+        if(i!=0):
+            try:
+                sma_star_bp_log.append(math.log10(s))
+            except:
+                # print i
+                sma_star_bp_log.append(sma_star_bp_log[-1])
+        i += 1
+
+    subplot.plot(times[1:],sma_starbp_ffp_log,c='red',label='FFP and Star+BP')
+    subplot.plot(times[1:],sma_star_bp_log,c='green',label='BP and Star')
 
     subplot.set_title('Semimajor Axis', fontsize=20)
     subplot.set_xlabel('$t$ (yr)', fontsize=20)
-    subplot.set_ylabel('$a$ (AU)', fontsize=20)
+    subplot.set_ylabel('$\log{(a)}$', fontsize=20)
     subplot.legend(fontsize=20)
 
     plt.savefig('./orbital_elements_'+filename+'.png')
     plt.close()
+
+def my_orbital_elements_from_binary(mass1, mass2, binary):
+
+    position = binary[1].position-binary[0].position
+    velocity = binary[1].velocity-binary[0].velocity
+    total_mass = mass1 + mass2
+
+    specific_energy = (1.0/2.0)*velocity.lengths_squared() - constants.G*total_mass/position.lengths()
+    specific_angular_momentum = position.cross(velocity)
+    specific_angular_momentum_norm = specific_angular_momentum.lengths()
+    specific_angular_momentum_unit=specific_angular_momentum/specific_angular_momentum_norm
+
+    semimajor_axis = -constants.G*total_mass/(2.0*specific_energy)
+
+    eccentricity_argument = 2.0*specific_angular_momentum_norm**2*specific_energy/(constants.G**2*total_mass**2)
+    if (eccentricity_argument <= -1): eccentricity = 0.0
+    else: eccentricity = np.sqrt(1.0 + eccentricity_argument)
+
+    ### Orbital inclination ###
+    inclination = np.degrees(np.arccos(specific_angular_momentum.z/specific_angular_momentum_norm))
+
+    ### Longitude of ascending nodes, with reference direction along x-axis ###
+    z_vector = [0.,0.,1.] | units.none
+    ascending_node_vector = z_vector.cross(specific_angular_momentum)
+    if ascending_node_vector.lengths().number==0:
+      ascending_node_vector_unit= np.array([1.,0.,0.])
+    else:
+      ascending_node_vector_unit = ascending_node_vector/ascending_node_vector.lengths()
+    long_asc_node=np.degrees(np.arctan2(ascending_node_vector_unit[1],ascending_node_vector_unit[0]))
+
+    ### Argument of periapsis and true anomaly, using eccentricity a.k.a. Laplace-Runge-Lenz vector ###
+    mu = constants.G*total_mass ### Argument of pericenter ###
+    position_unit = position/position.lengths()
+    e_vector = ( (1.0/mu)*velocity.cross(specific_angular_momentum) - position_unit ) | units.none
+    if (e_vector.lengths() == 0.0): ### Argument of pericenter and true anomaly cannot be determined for e = 0, in this case return 1.0 for the cosines ###
+        cos_arg_per = 1.0
+        arg_per=0.
+        cos_true_anomaly = 1.0
+        true_anomaly=0.
+    else:
+        e_vector_unit = e_vector/e_vector.lengths()
+
+        cos_arg_per = np.dot(e_vector_unit,ascending_node_vector_unit)
+        #cos_arg_per = e_vector_unit.dot(ascending_node_vector_unit)
+        e_cross_an=np.cross(e_vector_unit,ascending_node_vector_unit)
+        ss=-np.sign(np.dot(specific_angular_momentum_unit,e_cross_an))
+        #ss=-np.sign(specific_angular_momentum_unit.dot(e_cross_an))
+        sin_arg_per = ss*(e_cross_an**2).sum()**0.5
+        arg_per=np.degrees(np.arctan2(sin_arg_per,cos_arg_per))
+
+
+        cos_true_anomaly = np.dot(e_vector_unit,position_unit)
+        #cos_true_anomaly = e_vector_unit.dot(position_unit)
+        e_cross_pos=np.cross(e_vector_unit,position_unit)
+        ss=np.sign(np.dot(specific_angular_momentum_unit,e_cross_pos))
+        #ss=np.sign(specific_angular_momentum_unit.dot(e_cross_pos))
+        sin_true_anomaly = ss*(e_cross_pos**2).sum()**0.5
+        true_anomaly=np.degrees(np.arctan2(sin_true_anomaly,cos_true_anomaly))
+
+    return mass1, mass2, semimajor_axis, eccentricity, true_anomaly, inclination, long_asc_node, arg_per
 
 def make_individual_plots(filenames, ms_bp, as_bp, bs_ffp, phis_bp):
 
@@ -157,6 +239,10 @@ def make_individual_plots(filenames, ms_bp, as_bp, bs_ffp, phis_bp):
         xs = []
         ys = []
         times = []
+        snapshot = 1
+
+        x_start_longterm = [0.0,0.0,0.0]
+        y_start_longterm = [0.0,0.0,0.0]
 
         for data in bodies.history:
 
@@ -173,7 +259,25 @@ def make_individual_plots(filenames, ms_bp, as_bp, bs_ffp, phis_bp):
             ys.append(y_values)
             times.append(t_value)
 
-        plot_trajectory(np.array(xs), np.array(ys), filename)
+            if(snapshot==600):
+                x_start_longterm = x_values
+                y_start_longterm = y_values
+
+                m0 = 0.58 | units.MSun
+                m_ffp = 7.5 | units.MJupiter
+
+                star_ffp = [data[0], data[1]]
+                star_bp = [data[0], data[2]]
+
+                print 'mass1, mass2, semimajor_axis, eccentricity, true_anomaly, inclination, long_asc_node, arg_per'
+                m0, m_ffp, sma, ecc, ta, inc, lan, ap = my_orbital_elements_from_binary(m0, m_ffp, star_ffp)
+                print m0.value_in(units.MSun), m_ffp.value_in(units.MJupiter), sma.value_in(units.AU), ecc, ta, inc, lan, ap
+                m0, m_bpp, sma, ecc, ta, inc, lan, ap = my_orbital_elements_from_binary(m0, m_bp | units.MJupiter, star_bp)
+                print m0.value_in(units.MSun), m_bpp.value_in(units.MJupiter), sma.value_in(units.AU), ecc, ta, inc, lan, ap
+
+            snapshot += 1
+
+        plot_trajectory(np.array(xs), np.array(ys), filename, x_start_longterm, y_start_longterm)
         plot_orbital_elements(np.array(times), np.array(eccs), np.array(smas), filename)
 
         print is_hill_stable([607,7.5,m_bp], a_values, e_values)
